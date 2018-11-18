aed13a66783fcf2624a02127ab3c84195c7eae35
Abhijit Dutta <eddie@LAPTOP-NEU0ITND.localdomain>
2018-11-18 17:01:20 +0800

    added numpy and TensorFlow basics

---
+++ mySecondMagic.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+fruits = ['apple', 'mango', 'kiwi', 'watermelon', 'pear']
+fruits_len = len(fruits)
+fruits_len
+mp = ['m', 'p']
+mp
+fruits_mp = [word for word in fruits for l in mp if word[0] == l ]
+fruits_mp
---
+++ myfirstmagic.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+fruits = ['apple', 'mango', 'kiwi', 'watermelon', 'pear']
+fruits
+fruits_len = fruits.len
+fruits_len = fruits.len()
+fruits_len = len(fruits)
+print (fruits_len)
